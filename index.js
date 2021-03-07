b8946454151071553cc902435aca5716f2f7913a
Roberto Lai <epictetusrl@yahoo.com>
2021-03-07 18:09:52 +0100

    added web interface

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
+// REDUX code
+let todo_id = 0;
 const store = Redux.createStore(reducer, (state = []));
-const unsubscribe = store.subscribe(() => {
-  console.log("state:", store.getState());
-});
+const unsubscribe = store.subscribe(() => render(store.getState()));
 
-store.dispatch({ type: "todos/add", payload: { id: 1, name: "learn Angular" } });
-store.dispatch({ type: "todos/add", payload: { id: 2, name: "learn Kubernetes" } });
-store.dispatch({ type: "todos/add", payload: { id: 3, name: "learn Russian" } });
-store.dispatch({ type: "todos/add", payload: { id: 4, name: "learn Haskell" } });
-store.dispatch({ type: "todos/add", payload: { id: 5, name: "single swing 40Kg kettlebells" } });
-store.dispatch({ type: "todos/delete", payload: { id: 3 } });
-store.dispatch({ type: "todos/delete", payload: { id: 2 } });
+store.dispatch({ type: "todos/add", payload: { id: newIndex(), name: "learn Angular" } });
+store.dispatch({ type: "todos/add", payload: { id: newIndex(), name: "learn Kubernetes" } });
+store.dispatch({ type: "todos/add", payload: { id: newIndex(), name: "learn Russian" } });
+store.dispatch({ type: "todos/add", payload: { id: newIndex(), name: "learn Haskell" } });
+store.dispatch({ type: "todos/add", payload: { id: newIndex(), name: "single swing 40Kg kettlebells" } });
 
-unsubscribe();
+// unsubscribe()
 
 function reducer(state, action) {
   switch (action.type) {
@@ -21,7 +19,7 @@ function reducer(state, action) {
     case "todos/delete":
       return applyTodoDelete(state, action.payload);
       break;
-    // add other action here
+    // add other actions here
     default:
       return state;
   }
@@ -34,4 +32,37 @@ function applyTodoAdd(state, payload) {
 function applyTodoDelete(state, payload) {
   // NB: filter is immutable
   return state.filter(todo => todo.id !== payload.id);
+}
+
+// simple todo id creator
+function newIndex() {
+  todo_id = todo_id + 1;
+  return todo_id;
+}
+
+// WEB Interface code
+function render(state) {
+  const stateToHTML = state
+    .map(
+      todo =>
+        `<input class='btnDelete red' type='button' onclick='deleteTodo(${todo.id})' value='-'>
+        <span class='todo'>${todo.name}</span>`
+    )
+    .join("<br>");
+
+  const todosHTML = document.getElementById("todos");
+  todosHTML.innerHTML = stateToHTML;
+
+  const stateHTML = document.getElementById("state");
+  stateHTML.innerHTML = state.map(todo => JSON.stringify(todo)).join("<br>");
+}
+function deleteTodo(id) {
+  const action = { type: "todos/delete", payload: { id: id } };
+  store.dispatch(action);
+}
+function addTodo() {
+  const todo = document.getElementById("mytodo").value;
+  document.getElementById("mytodo").value = "";
+  const action = { type: "todos/add", payload: { id: newIndex(), name: todo } };
+  store.dispatch(action);
 }
